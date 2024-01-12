3be2dda169a44ddf006307ad7aaa2aea202c5190
Kevin Wu <kevinwu098@gmail.com>
2024-01-12 02:04:57 -0800

    fix: simplify main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-import HeroContent from "@/components/HeroContent";
-import LoginComponent from "@/components/LoginComponent";
 import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { Loader2, Square } from "lucide-react";
+import Link from "next/link";
 
 export default async function Home() {
     const { getUser } = getKindeServerSession();
@@ -16,53 +15,16 @@ export default async function Home() {
           })
         : null;
 
-    const dbCalendar = user
-        ? await db.calendar.findFirst({
-              where: {
-                  userId: user.id,
-              },
-              include: {
-                  weekSquares: true,
-              },
-          })
-        : null;
-
-    const birthday = dbUser?.birthday === undefined ? null : dbUser.birthday;
-    const finalYear = dbUser?.finalYear === undefined ? null : dbUser.finalYear;
-    const weekSquares =
-        dbCalendar?.weekSquares === undefined ? null : dbCalendar.weekSquares;
-
     return (
-        <main className="wrapper mt-24 flex flex-col gap-x-24 min-h-[calc(100vh-6rem)]">
-            <div>
-                <div
-                    className="relative -ml-2.5 h-fit w-fit"
-                    aria-hidden={true}
-                >
-                    <Square fill="text-primary" size={112} />
-                    <div className="absolute left-1/2 top-1/2 z-10 h-fit w-fit -translate-x-1/2 -translate-y-1/2 transform">
-                        <Loader2
-                            className="animate-spin text-white duration-50000"
-                            fill="text-primary"
-                            size={64}
-                        />
-                    </div>
-                </div>
-                <div className="w-fit gap-y-8 mb-2 flex flex-col justify-between gap-x-24">
-                    <div className="flex flex-col gap-8">
-                        <h1 className="text-7xl font-bold whitespace-nowrap">
-                            {user ? `${user.given_name}'s ` : null}Life, Squared
-                        </h1>
-                    </div>
-                    <LoginComponent />
-                </div>
-            </div>
-
-            <HeroContent
-                dbBirthday={birthday}
-                dbFinalYear={finalYear}
-                dbWeekSquares={weekSquares}
-            />
+        <main className="wrapper flex-center text-3xl md:text-5xl font-semibold min-h-[calc(100vh-6rem)]">
+            <Link
+                href={"https://github.com/KevinWu098/kTemp"}
+                target="_blank"
+                referrerPolicy="no-referrer"
+                className="underline"
+            >
+                Hello World 💖
+            </Link>
         </main>
     );
 }

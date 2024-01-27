c3af9370e6041e7cea20c45f6ef97bae725d69c4
Kevin Wu <kevinwu098@gmail.com>
2024-01-26 18:39:42 -0800

    feat: disable getUser on default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
+import Link from "next/link";
 import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { Loader2, Square } from "lucide-react";
-import Link from "next/link";
 
 export default async function Home() {
-    const { getUser } = getKindeServerSession();
-    const user = await getUser();
+    // const { getUser } = getKindeServerSession();
+    // const user = await getUser();
 
-    const dbUser = user
-        ? await db.user.findFirst({
-              where: {
-                  id: user?.id,
-              },
-          })
-        : null;
+    // const dbUser = user
+    //     ? await db.user.findFirst({
+    //           where: {
+    //               id: user?.id,
+    //           },
+    //       })
+    //     : null;
 
     return (
         <main className="wrapper flex-center text-3xl md:text-5xl font-semibold min-h-[calc(100vh-6rem)]">
